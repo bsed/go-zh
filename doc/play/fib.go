0bd90f0b06e0bc514463ede958f87d72544b17a7
Shenghou Ma <minux.ma@gmail.com>
2012-08-29 23:43:07 +0800

    doc/reference: ??docs?"????"

---
+++ doc/play/fib.go
@@ -0,0 +1,16 @@
+package main
+
+// fib 返回一个函数，该函数返回连续的斐波纳契数。
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func main() {
+	f := fib()
+	// 函数调用按从左到右顺序求值。
+	println(f(), f(), f(), f(), f())
+}
